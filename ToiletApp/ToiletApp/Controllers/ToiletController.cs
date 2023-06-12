96df9f00ea30f808d5ff47956f745c574bf2d42d
benedek.csikos <80459242+Csiki10@users.noreply.github.com>
2023-06-12 18:54:30 +0200

    app added

---
+++ ToiletApp/ToiletApp/Controllers/ToiletController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using ToiletApp.Models;
+
+namespace ToiletApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToiletController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Toilet> GetToilets()
+        {
+            return repository.ListToilets();
+        }
+
+        [HttpGet("{id}")]
+        public Toilet? GetToilet(string id)
+        {
+            return repository.GetToilets(id);
+        }
+
+        [HttpPost]
+        public void AddToilet([FromBody] Toilet t)
+        {
+            repository.AddToilet(t);
+        }
+
+        [HttpPut]
+        public void EditToilet([FromBody] Toilet t)
+        {
+            repository.EditToilet(t);
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteToilet(string id)
+        {
+            repository.Delete(id);
+        }
+    }
+}
---
+++ ToiletApp/ToiletApp/Models/Toilet.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+using System.Xml.Linq;
+namespace ToiletApp.Models
+{
+    public enum Institution
+    {
+        [Display(Name = "Mc Donald's")] McDonalds,
+        [Display(Name = "Burger King")] BurgerKing,
+        [Display(Name = "Costa Caffe")] CostaCaffe,
+        [Display(Name = "Star bucks")] StarBucks,
+        [Display(Name = "Frei Caffe")] FreiCaffee,
+        [Display(Name = "KFC")] KFC,
+        [Display(Name = "Pizza Hut")] PizzaHut,
+        [Display(Name = "Other")] Other
+    }
+    public class Toilet
+    {
+        [Key]
+        public string Uid { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "Maximum length is 20 characters.")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Code must be minimum 2 letter long.")]
+        public string Code { get; set; }
+
+        public virtual SiteUser? User { get; set; }
+        public string UserId { get; set; }
+
+        [NotMapped]
+        public virtual Address? Address { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Opinion> Opinions { get; set; }
+
+        [Required]
+        public Institution Institution { get; set; }
+
+        public string? ImageFileName { get; set; }
+
+        public string? ContentType { get; set; }
+
+        public byte[]? Data { get; set; }
+
+        public Toilet()
+        {
+            Uid = Guid.NewGuid().ToString();
+            Opinions = new HashSet<Opinion>();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Toilet toilet &&
+                   Code == toilet.Code &&
+                   Address.Equals(toilet.Address) &&
+                   Institution == toilet.Institution;
+        }
+    }
+}
